fc45866563d8604985bdfe50137ca86736c51803
Michele Gargiulo <michele@michelegargiulo.info>
2015-09-08 00:38:15 +0200

    Added comments

--- chess/utils.py
+++ chess/utils.py
@@ -2,6 +2,19 @@ from piece import *
 
 
 def create_pieces_list(kings, queens, rooks, bishops, knights):
+    """Creates the list of pieces to loop.
+
+    :param kings: Number of kings pieces
+    :type kings: int
+    :param queens: Number of queens pieces
+    :type queens: int
+    :param rooks: Number of rooks pieces
+    :type rooks: int
+    :param bishops: Number of bishops pieces
+    :type bishops: int
+    :param knights: Number of knights pieces
+    :type knights: int
+    """
     pieces = []
 
     for queen in range(queens):
